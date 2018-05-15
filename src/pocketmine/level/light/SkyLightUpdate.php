ad706b519d82b2419f29eee2591030b81fc388dc
egr7v8 <egrrub907@gmail.com>
2018-05-15 22:55:17 +0300

    Removed shadow.

--- src/pocketmine/level/light/SkyLightUpdate.php
+++ src/pocketmine/level/light/SkyLightUpdate.php
@@ -12,10 +12,10 @@ namespace pocketmine\level\light;
 class SkyLightUpdate extends LightUpdate
 {
     public function getLight(int $x, int $y, int $z) : int{
-        return $this->subChunkHandler->currentSubChunk->getBlockSkyLight($x & 0x0f, $y & 0x0f, $z & 0x0f);
+        return $this->level->getBlockSkyLightAt($x & 0x0f, $y & 0x0f, $z & 0x0f);
     }
 
     public function setLight(int $x, int $y, int $z, int $level){
-        $this->subChunkHandler->currentSubChunk->setBlockSkyLight($x & 0x0f, $y & 0x0f, $z & 0x0f, $level);
+        $this->level->setBlockSkyLightAt($x & 0x0f, $y & 0x0f, $z & 0x0f, $level);
     }
 }
